38b0d329ffb983a1f7b75c729d8251e9d9fd3a60
Steve Shreeve <steve.shreeve@gmail.com>
2017-03-06 04:40:00 -0700

    Massive simplification, just needs some testing

--- lib/bindings.rb
+++ lib/bindings.rb
@@ -17,33 +17,14 @@ require 'fiddle/import'
 module Fiddle
   module Binding
     extend Fiddle::Importer
-
     dlload Fiddle.dlopen(nil)
-
-    extern "void* rb_debug_inspector_open(void*, void*)"
-    bind("void* callback(void*, void*)") {|ptr, _| DebugStruct.new(ptr).contexts }
-
-    DebugStruct = struct [
-      "void* thread",
-      "void* frame",
-      "void* backtrace",
-      "void* contexts",
-      "long  backtrace_size"
-    ]
-
-    class << self
-      def callers
-        list = rb_debug_inspector_open(self['callback'], nil).to_value
-        list.drop(4).map {|ary| ary[2]}
-      end
-    end
+    rb_debug_inspector_open              = extern "void* rb_debug_inspector_open(void*, void*)"
+    rb_debug_inspector_frame_binding_get = extern "void* rb_debug_inspector_frame_binding_get(void*, long)"
+    callback = bind("void* callback(void*, void*)") {|ctx, n| rb_debug_inspector_frame_binding_get.call(ctx, n+4)}
   end
 end
 
-class ::Binding
-  def of_caller(n)
-    c = ::Fiddle::Binding.callers
-    n < c.size or raise "No such frame, gone beyond end of stack!"
-    c[n]
-  end
+class Binding
+  def self.of_caller(n); ::Fiddle::Binding.rb_debug_inspector_open(::Fiddle::Binding['callback'], n).to_value; end
+  def      of_caller(n); self.class.of_caller(n); end
 end
